8ae0ad755cbe40494a918665da3c1b205e225728
OneGoodReas0n <korobovi4@gmail.com>
2019-11-01 21:04:51 +0200

    init: implementing webpack config file for building project

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './scripts/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'build.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: 'css-loader',
+                    },
+
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ]
+            }
+
+        ],
+
+        
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ],
+
+    mode: 'development'
+}
