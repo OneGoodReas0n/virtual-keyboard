dbab780b899fefbd18043ca3722c6c9fe2cda256
OneGoodReas0n <korobovi4@gmail.com>
2019-11-03 23:17:56 +0200

    update: write a logic for keyboard layout and replace a logic to keyboard-data.js for creating a keyboard elements

--- src/index.js
+++ src/index.js
@@ -1,41 +1,18 @@
-/* eslint-disable no-nested-ternary */
-import { englishKeyboard } from './keyboard/keyboard-data'
+import './styles/style.scss';
+import keyboard from './keyboard/keyboard-data';
 
-console.log(englishKeyboard)
+window.onload = () => {
+    const { body } = document;
 
-const { body } = document
+    const containerDiv = document.createElement('div');
+    containerDiv.classList.add('container');
 
-const containerDiv = document.createElement('div')
-containerDiv.classList.add('container')
+    const textarea = document.createElement('textarea');
+    textarea.classList.add('container__textarea');
 
-const textarea = document.createElement('textarea')
-textarea.classList.add('container__textarea')
+    containerDiv.appendChild(textarea);
 
-containerDiv.appendChild(textarea)
+    containerDiv.appendChild(keyboard);
 
-const keyboardDiv = document.createElement('div')
-keyboardDiv.classList.add('keyboard')
-
-const rowDiv = document.createElement('div')
-rowDiv.classList.add('row')
-
-const btnDiv = document.createElement('div')
-btnDiv.classList.add('btn-key')
-btnDiv.classList.add('btn-key-letter')
-
-const spanElem = document.createElement('span')
-spanElem.classList.add('text')
-spanElem.innerHTML = '$'
-
-btnDiv.appendChild(spanElem)
-
-rowDiv.appendChild(btnDiv)
-
-keyboardDiv.appendChild(rowDiv)
-
-containerDiv.appendChild(keyboardDiv)
-
-body.appendChild(containerDiv)
-
-const result = [1, 2, 3].map(e => e + 1)
-console.log(result)
+    body.appendChild(containerDiv);
+};
