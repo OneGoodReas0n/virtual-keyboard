0af521638ad252d4882a484c38c9e9ab3381bdc0
OneGoodReas0n <korobovi4@gmail.com>
2019-11-06 20:58:34 +0200

    refactor: reorganization code for better readability

--- src/index.js
+++ src/index.js
@@ -1,143 +1,40 @@
 import './styles/style.scss';
-import Keyboard from './keyboard/keyboard-data';
+import Keyboard from './keyboard';
+
+const keyboard = new Keyboard();
 
 window.onload = () => {
-    const { body } = document;
+    if (localStorage.getItem('lang').length === 0) {
+        localStorage.setItem('lang', 'en');
+    }
 
-    let lang = 'en';
+    const { body } = document;
 
     const containerDiv = document.createElement('div');
     containerDiv.classList.add('container');
 
+    const textareaDiv = document.createElement('div');
+    textareaDiv.classList.add('textarea-wrapper');
+
     const textareaElem = document.createElement('textarea');
     textareaElem.id = 'textarea';
-    textareaElem.classList.add('container__textarea');
+    textareaElem.classList.add('textarea-wrapper__textarea');
 
-    containerDiv.appendChild(textareaElem);
+    textareaDiv.appendChild(textareaElem);
 
-    containerDiv.appendChild(Keyboard.renderKeyboard(lang, false));
+    containerDiv.appendChild(textareaDiv);
 
-    const changeLang = {
-        ctrl: false,
-        alt: false,
-    };
-
-    const keyUpHandler = e => {
-        let virtual = true;
-        let name = e.id;
-
-        if (e.code !== undefined) {
-            name = e.code;
-            virtual = false;
-        }
-        if (!virtual) {
-            if (name.includes('Shift')) {
-                while (containerDiv.children.length > 1) {
-                    containerDiv.removeChild(containerDiv.lastChild);
-                }
-                containerDiv.appendChild(Keyboard.renderKeyboard(lang, false));
-            } else if (String(name).includes('Control')) {
-                changeLang.ctrl = false;
-            } else if (String(name).includes('Alt')) {
-                changeLang.alt = false;
-            }
-        }
-        const elem = document.getElementById(name);
-        setTimeout(() => {
-            elem.classList.remove('btn-key_active');
-        }, 100);
-    };
-
-    const keyDownHandler = e => {
-        const textarea = document.getElementById('textarea');
-        textarea.focus();
-        let name = e.id;
-        let virtual = true;
-        const virtualShift = false;
-        if (e.code !== undefined) {
-            name = e.code;
-            virtual = false;
-        }
-        if (String(name).includes('Digit') || String(name).includes('Key') || specialKeys.includes(name)) {
-            if (!virtual) {
-                e.preventDefault();
-            }
-            textarea.value += document.getElementById(name).children.item(0).innerHTML;
-        } else if (String(name).includes('Tab')) {
-            if (!virtual) {
-                e.preventDefault();
-            }
-            textarea.value += '   ';
-        } else if (name.includes('Shift') && virtual) {
-            while (containerDiv.children.length > 1) {
-                containerDiv.removeChild(containerDiv.lastChild);
-            }
-            if (virtualShift) {
-                containerDiv.appendChild(Keyboard.renderKeyboard(lang, false));
-            } else {
-                containerDiv.appendChild(Keyboard.renderKeyboard(lang, true));
-            }
-        } else if (name.includes('Shift') && !virtual) {
-            while (containerDiv.children.length > 1) {
-                containerDiv.removeChild(containerDiv.lastChild);
-            }
-            containerDiv.appendChild(Keyboard.renderKeyboard(lang, true));
-        } else if (String(name).includes('Control')) {
-            changeLang.ctrl = true;
-        } else if (String(name).includes('Alt')) {
-            if (!virtual) {
-                e.preventDefault();
-            }
-            changeLang.alt = true;
-        }
-        if (changeLang.alt && changeLang.ctrl) {
-            lang = toggleLang(lang);
-            while (containerDiv.children.length > 1) {
-                containerDiv.removeChild(containerDiv.lastChild);
-            }
-            containerDiv.appendChild(Keyboard.renderKeyboard(lang, false));
-        }
-        const elem = document.getElementById(name);
-        elem.classList.add('btn-key_active');
-    };
+    containerDiv.appendChild(keyboard.renderKeyboard(localStorage.getItem('lang')));
 
     document.addEventListener('keydown', e => {
-        keyDownHandler(e);
+        keyboard.keyDownHandler(e, containerDiv);
     });
 
     document.addEventListener('keyup', e => {
-        keyUpHandler(e);
+        keyboard.keyUpHandler(e, containerDiv);
     });
 
-    containerDiv.lastChild.childNodes.forEach(row => {
-        row.childNodes.forEach(btn => {
-            btn.addEventListener('click', () => {
-                keyDownHandler(btn);
-                setTimeout(keyUpHandler(btn), 200);
-            });
-        });
-    });
+    keyboard.addVirtualHandler(containerDiv);
 
     body.appendChild(containerDiv);
 };
-
-const toggleLang = lang => {
-    if (lang === 'en') {
-        return 'ru';
-    }
-    return 'en';
-};
-
-const specialKeys = [
-    'Backquote',
-    'Minus',
-    'Equal',
-    'Backslash',
-    'Semicolon',
-    'Quote',
-    'Comma',
-    'Period',
-    'Slash',
-    'BracketLeft',
-    'BracketRight',
-];
